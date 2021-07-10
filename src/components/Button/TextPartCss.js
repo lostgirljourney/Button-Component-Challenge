26311fe0ab3403196653415c240fe0dda093a8dc
lostgirljourney <falgunisarkar526@gmail.com>
2021-07-10 20:26:31 +0530

    Updates

--- src/components/Button/TextPartCss.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import './TextPart.css';
-
-export default function TextPartCss(textCss) {
-    return (
-        <p className="textcss">{textCss.text}</p>
-    );
-};
--- src/components/Button/TextPartMain.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import './TextPart.css';
-
-export default function TextPartMain(textMain) {
-    return (
-        <p className="textmcss">{textMain.text}</p>
-    );
-};
